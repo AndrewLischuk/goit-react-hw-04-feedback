e4715e993f28384d2bf0f4bf0094de0ef7bc29f1
Andrew Lischuk <andrewlischuk59@gmail.com>
2022-10-28 11:22:20 +0300

    fix Section.module.css

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,7 +2,7 @@ import styles from './Section.module.css';
 
 export const Section = ({ title, children }) => {
   return (
-    <div>
+    <div className={styles.section}>
       <h2>{title}</h2>
       {children}
     </div>
